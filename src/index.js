b309e71b90a070b018fa495cc40cdfdf59b825e3
ravi samra <ravinangal3@gmail.com>
2023-09-18 17:45:13 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+import Board from './App.js';
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Board />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
